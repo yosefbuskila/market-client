cdb2db3b15a114cb79b5743cfc0d4dfe557ea8a5
yosefbuskila <yosef3053@gmail.com>
2019-02-21 17:07:45 +0200

    21/2/19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
