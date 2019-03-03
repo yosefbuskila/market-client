720ec5406171f1b13ddea0eca70608d1a88c8973
yosef buskila <yosef3053@gmail.com>
2019-03-03 22:28:40 +0200

    3/3/19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component'
 import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'logIn', component: LogInComponent }
+  { path: 'logIn', component: LogInComponent },
+  { path: 'reg', component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  triedFirst=false;
+  triedsec=false;
+  firstSwitch=false;
+  profileForm = this.fb.group({
+    firstForm: this.fb.group({
+      id: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    }),
+    secForm: this.fb.group({
+      city: [''],
+      street: [''],
+      firstName: [''],
+      lastName: ['']
+    }),
+  });
+  firstForm=this.profileForm.controls.firstForm['controls'];
+  secForm=this.profileForm.controls.secForm['controls'];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
-
+  next() {
+    console.log(this.firstForm)
+    this.triedFirst=true
+  }
+  onSubmit(){
+    this.triedsec=true;
+  }
 }
